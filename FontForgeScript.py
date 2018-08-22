cb93c431b786cc9574ff544c20a15a5e3ef45ee9
hunterwb <hunter@hunterwb.com>
2018-08-22 00:02:03 -0700

    Resize and hint otf font

--- FontForgeScript.py
+++ FontForgeScript.py
@@ -6,9 +6,16 @@ for fileName in sys.argv[1::]:
     fontNameTtf = fontNameBase + '.ttf'
     fontNameOtf = fontNameBase + '.otf'
     print(fontNameBase)
+
     font = fontforge.open(fontNameTtf)
     for glyph in font.glyphs():
         glyph.removeOverlap()
         glyph.simplify()
-    font.generate(fontNameOtf, flags='short-post')
+
     font.generate(fontNameTtf, flags='short-post')
+
+    font.em = 1000
+    font.private['BlueValues'] = []
+    font.private['BlueScale'] = 0.0
+    font.private['BlueFuzz'] = 0.0
+    font.generate(fontNameOtf, flags='short-post')
